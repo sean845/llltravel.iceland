2c6696755e1b5ba6c7c7121afa44b03aee8dd094
sean845 <74813186+sean845@users.noreply.github.com>
2021-04-07 20:45:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,4 +7,11 @@
        humbugerMenu.classList.toggle('humbuger-show')
        line.classList.toggle('spin');
    });
+
+   const contact = document.getElementById('contact');
+   const snsMenu = document.getElementById('sns-menu');
+
+   contact.addEventListener('click', () => {
+       snsMenu.classList.toggle('sns-show');
+   });
 }
