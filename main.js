5d624af8e9cafb5d8c4bedb97cbcc48b90da5a65
sean845 <74813186+sean845@users.noreply.github.com>
2021-04-10 09:31:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
    const menuIcon = document.getElementById('humbuger');
    const humbugerMenu = document.getElementById('humbuger-menu');
    const line = document.getElementById('line');
+
    menuIcon.addEventListener('click', () => {
        humbugerMenu.classList.toggle('humbuger-show')
        line.classList.toggle('spin');
